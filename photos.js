2300e2273401b7227a66a9d0b2ed2816170c2b8c
vrigue <vrielleguevarra@gmail.com>
2024-02-24 11:03:23 -0500

    photo gallery, arrows are not done

--- photos.js
+++ photos.js
@@ -1,31 +1,35 @@
 const colors = ["purple", "pink", "blue"];
-var currentColor = 0;
+var current_color = 0;
 
 function getPreviousContent() {
-    previousColor = (currentColor - 1) % 3;
-    showContent(previousColor);
+    previous_color = ((current_color - 1) % 3);
+    if (previous_color == -1) { previous_color = 2; }
+    showContent(previous_color);
 }
   
 function getNextContent() {
-    nextColor = (currentColor + 1) % 3;
-    showContent(nextColor);
+    next_color = ((current_color + 1) % 3);
+    showContent(next_color);
+}
+
+function clearContent(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
 }
 
 function showContent(color) {
-  var content = document.querySelectorAll('.content');
   var image_container = document.querySelectorAll('.image-container');
 
-  while (image_container.firstChild) {
-    image_container.removeChild(image_container.firstChild);
-  }
+  clearContent(image_container[0]);
 
   var path = colors[color];
 
   for (num = 0; num < 30; num++) {
     let img = document.createElement("img");
-    img.src = "photos-images/" + path + "/" + num + ".jpg";
+    img.src = "photos-images/" + path + "/" + num + ".jpeg";
     image_container[0].appendChild(img);
   }
 
-  currentColor = color;
+  current_color = color;
 }
