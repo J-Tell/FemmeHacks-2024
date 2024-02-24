0e06b10421d7c9f0a8e234b8a503c2fcf66c7045
Julia <jewelst2014@gmail.com>
2024-02-24 10:15:06 -0500

    fixed pomodoro

---
+++ timer.js
@@ -0,0 +1,53 @@
+let workTag = document.getElementById('work');
+let breakTag = document.getElementById('break');
+
+let workTime = 25;
+let breakTime = 5;
+let seconds = "00"
+
+window.onload = () => {
+    document.getElementById('minutes').innerHTML = workTime;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    workTag.classList.add('active');
+}
+
+function start() {
+    document.getElementById('start').style.display = "none";
+    document.getElementById('reset').style.display = "block";
+
+    seconds = 59;
+    let workMinutes = workTime - 1;
+    let breakMinutes = breakTime - 1;
+
+    breakCount = 0;
+
+    let timerFunction = () => {
+        document.getElementById('minutes').innerHTML = workMinutes;
+        document.getElementById('seconds').innerHTML = seconds;
+
+        seconds = seconds - 1;
+
+        if (seconds === 0) {
+            workMinutes = workMinutes - 1;
+            if (workMinutes === -1 ) {
+                if (breakCount % 2 === 0) {
+                    workMinutes = breakMinutes;
+                    breakCount++
+
+                    workTag.classList.remove('active');
+                    breakTag.classList.add('active');
+                } else {
+                    workMinutes = workTime;
+                    breakCount++;
+
+                    breakTag.classList.remove('active');
+                    workTag.classList.add('active');
+                }
+            }
+            seconds = 59;
+        }
+    }
+
+    setInterval(timerFunction, 1000);
+}
